fb588e78e648fd8ebd55b3efff6417278d495364
WangHaoMing <w18203235565@163.com>
2022-05-01 18:26:25 +0800

    hot-fix commit1

---
+++ src/main/java/com/demomybatisplus/config/Test.java
@@ -0,0 +1,14 @@
+package com.demomybatisplus.config;
+
+/**
+ * @program: demo-mybatis-plus
+ * @description: x
+ * @author: wanghaoming
+ * @create: 2022-05-01 18:25
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("xxx");
+    }
+}
